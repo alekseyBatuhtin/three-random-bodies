63de7417feb5e73f77a4316e4a0ed6d2d1e37ab1
Aleksey Batuhtin <full.naked.eye@gmail.com>
2022-11-01 19:02:21 +0400

    add random body generator

--- src/body/mod.rs
+++ src/body/mod.rs
@@ -1,6 +1,7 @@
 pub mod body {
     use crate::trajectory::trajectory::TrajectoryPoint;
-
+    use rand::{thread_rng, Rng};
+    use std::f32::consts::PI;
     #[derive(Debug)]
     pub struct Body {
         pub dot_position: TrajectoryPoint,
@@ -9,12 +10,39 @@ pub mod body {
     }
 
     impl Body {
-        pub fn new(x: f32, y: f32, z: f32, mass: u32) -> Self {
-            Body {
-                dot_position: TrajectoryPoint { x, y, z },
+        pub fn new(x: f32, y: f32, z: f32, dot_x: f32, dot_y: f32, dot_z: f32, mass: u32) -> Self {
+            Self {
+                dot_position: TrajectoryPoint {
+                    x: dot_x,
+                    y: dot_y,
+                    z: dot_z,
+                },
                 mass,
                 trajectory: vec![TrajectoryPoint { x, y, z }],
             }
+        }
+
+        pub fn gen_random_body() -> Self {
+            let mut rng = thread_rng();
+
+            let mass: u32 = rng.gen_range(1..=100);
+            let position: TrajectoryPoint = TrajectoryPoint {
+                x: rng.gen_range(-10.0..=10.0),
+                y: rng.gen_range(-10.0..=10.0),
+                z: rng.gen_range(-10.0..=10.0),
+            };
+
+            let velocity: f32 = rng.gen_range(0.0..=1.0);
+            let (theta, sigma): (f32, f32) =
+                (rng.gen_range(0.0..=2.0 * PI), rng.gen_range(0.0..=2.0 * PI));
+
+            let dot_x = velocity * theta.sin() * sigma.cos();
+            let dot_y = velocity * theta.sin() * sigma.sin();
+            let dot_z = velocity * theta.cos();
+
+            Self::new(
+                position.x, position.y, position.z, dot_x, dot_y, dot_z, mass,
+            )
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod trajectory;
 use crate::body::body::Body;
 
 fn main() {
-    let body = Body::new(1.0, 1.0, 1.0, 100);
+    let body = Body::gen_random_body();
     println!("{:?}", body);
 }
